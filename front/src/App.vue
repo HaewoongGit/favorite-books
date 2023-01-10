0948bdc09a27ef20eae993947c7ff65175891666
HaewoongGit <107612118+HaewoongGit@users.noreply.github.com>
2023-01-10 23:20:24 +0900

    상단바, 상품목록 화면 추가

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,42 +1,17 @@
 <template>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-sparta justify-content-end">
-        <a class="navbar-brand" href="/goods">
-            <img src="./assets/logo_big_tr.png" width="30" height="30" class="d-inline-block align-top" alt="" /> 나의 쇼핑몰
-        </a>
-        <button
-            class="navbar-toggler ml-auto"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="true"
-            aria-label="Toggle navigation"
-        >
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse flex-grow-0 ml-auto" id="navbarSupportedContent" style="">
-            <ul class="navbar-nav mr-auto text-right">
-                <li class="nav-item" id="link-cart">
-                    <a class="nav-link" href="/cart"> 장바구니 </a>
+    <nav class="navbar navbar-expand-lg bg-topBar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="./assets/shopping.png" width="30" height="30" /> 아는 사람만 아는 쇼핑몰</a>
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">장바구니</a>
                 </li>
-                <li class="nav-item" id="link-logout">
-                    <a class="nav-link" data-toggle="modal" data-target="#signOutModal" style="cursor: pointer"> 로그아웃 </a>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">로그아웃</a>
                 </li>
             </ul>
         </div>
     </nav>
-    <div class="wrap">
-        <div>
-            <div class="form-group row mr-0">
-                <label for="categorySelect" class="col-4 col-form-label">카테고리</label>
-                <select class="form-control col-8" id="categorySelect">
-                    <option value="" selected>전체</option>
-                    <option value="drink">음료</option>
-                    <option value="food">음식</option>
-                </select>
-            </div>
-        </div>
-    </div>
 </template>
 
 <script>
---
+++ front/src/components/productsComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="wrap">
+        <div>
+            <div class="form-group row mr-0">
+                <label for="categorySelect" class="col-4 col-form-label">카테고리</label>
+                <select class="form-control col-8" id="categorySelect">
+                    <option value="" selected>전체</option>
+                    <option value="drink">음료</option>
+                    <option value="food">음식</option>
+                </select>
+            </div>
+        </div>
+        <div id="goodsList" class="mb-5">
+            <div>
+                <div class="card mb-2" onclick="location.href='#'">
+                    <div class="row no-gutters">
+                        <div class="col-sm-5" style="background: #868e96">
+                            <img
+                                src="https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg"
+                                class="card-img-top h-100"
+                                alt="..."
+                            />
+                        </div>
+                        <div class="col-sm-7 d-flex">
+                            <div class="card-body flex-fill">
+                                <div class="card-title mb-auto">
+                                    <h5 style="display: inline">상품 1</h5>
+                                    <span class="card-price ml-2">$6.20</span>
+                                </div>
+                                <span class="badge badge-secondary">drink</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
