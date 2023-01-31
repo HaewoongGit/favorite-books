c2d5a18d1dbae4eed8676c802088d06d3c11a2a5
HaewoongGit <hw.lee3542@gmail.com>
2023-02-01 00:01:14 +0900

    크롤링 기능 추가

--- back/routers/goods.js
+++ back/routers/goods.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const Goods = require("../schemas/Goods");
-
 const Cart = require("../schemas/cart");
+
+const cheerio = require("cheerio");
+const axios = require("axios");
+const iconv = require("iconv-lite");
+const url =
+    "http://www.yes24.com/24/Category/BestSeller";
 
 const router = express.Router();
 
-router.get("/goods", async (req, res, next) => {
+router.get("/goods", async(req, res, next) => {
     try {
         if (Object.keys(req.query).length === 0) {
             const goods = await Goods.find({}).sort("-goodsId");
@@ -23,20 +28,20 @@ router.get("/goods", async (req, res, next) => {
     }
 });
 
-router.get("/goods/:goodsId", async (req, res) => {
+router.get("/goods/:goodsId", async(req, res) => {
     const { goodsId } = req.params;
     goods = await Goods.findOne({ goodsId: goodsId });
     res.json({ detail: goods });
 });
 
-router.post("/goods/sample", async (req, res) => {
+router.post("/goods/sample", async(req, res) => {
     const { sample } = req.body;
 
     await Goods.create(sample);
     res.send({ result: "success" });
 });
 
-router.post("/goods/:goodsId/cart", async (req, res) => {
+router.post("/goods/:goodsId/cart", async(req, res) => {
     try {
         let { goodsId } = req.params;
         let { quantity } = req.body;
@@ -56,7 +61,7 @@ router.post("/goods/:goodsId/cart", async (req, res) => {
     }
 });
 
-router.get("/cart", async (req, res) => {
+router.get("/cart", async(req, res) => {
     const cart = await Cart.find({});
     const goodsId = cart.map((cart) => cart.goodsId);
 
@@ -76,7 +81,7 @@ router.get("/cart", async (req, res) => {
     });
 });
 
-router.patch("/cart/update", async (req, res) => {
+router.patch("/cart/update", async(req, res) => {
     let { quantity, goodsId } = req.body;
 
     quantity = parseInt(quantity);
@@ -90,7 +95,7 @@ router.patch("/cart/update", async (req, res) => {
     res.send({ result: "/cart/update communication success" });
 });
 
-router.delete("/cart/delete/:goodsId", async (req, res) => {
+router.delete("/cart/delete/:goodsId", async(req, res) => {
     try {
         let { goodsId } = req.params;
 
@@ -106,6 +111,46 @@ router.delete("/cart/delete/:goodsId", async (req, res) => {
     } catch (err) {
         res.send({ result: err });
     }
+});
+
+router.get("/goods/add/crawling", async(req, res) => {
+
+    try {
+        await axios({
+            url: url,
+            method: "GET",
+            responseType: "arraybuffer",
+        }).then(async(html) => {
+            const content = iconv.decode(html.data, "EUC-KR").toString();
+            const $ = cheerio.load(content);
+            const list = $("ol li");
+            await list.each(async(i, tag) => {
+                let desc = $(tag).find("p.copy a").text()
+                let image = $(tag).find("p.image a img").attr("src")
+                let title = $(tag).find("p.image a img").attr("alt")
+                let price = $(tag).find("p.price strong").text()
+
+                if (desc && image && title && price) {
+                    price = price.slice(0, -1).replace(/(,)/g, "")
+                    let date = new Date()
+                    let goodsId = date.getTime()
+                    await Goods.create({
+                        goodsId: goodsId,
+                        name: title,
+                        thumbnailUrl: image,
+                        category: "도서",
+                        price: price
+                    })
+                }
+            })
+        });
+        res.send({ result: "success", message: "크롤링이 완료 되었습니다." });
+
+    } catch (error) {
+        console.log(error)
+        res.send({ result: "fail", message: "크롤링에 문제가 발생했습니다", error: error });
+    }
+
 });
 
 module.exports = router;
--- back/schemas/index.js
+++ back/schemas/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-    mongoose.connect("mongodb://localhost:27017/shopping-mall", {}).catch((err) => console.log(err));
+    mongoose.connect("mongodb://127.0.0.1:27017/shopping-mall", {}).catch((err) => console.log(err));
 };
 
 mongoose.connection.on("error", (err) => {
